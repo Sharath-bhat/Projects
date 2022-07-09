1731ba1c8bb23e1b0a30abf903c61c70534dd408
Ganapati Bhat <ganapati.anant-bhat@alstomgroup.com>
2022-07-09 17:01:35 +0530

    EmployeeApplication Project Added

---
+++ Amazon_ProductData/Amazon_ProductData.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv(r"C:\Users\427670\Documents\Projects\DataSets\Amazon_products\Amazon_Products.csv")
+
+print(data)
---
+++ EmployeeApllication/EmployeeDetails/App_Data/dbo.Employees.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeId] INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]  VARCHAR (25) NOT NULL,
+    [LastName]   VARCHAR (25) NOT NULL,
+    [MobileNo]   VARCHAR (15) NULL,
+    [EmailId]    VARCHAR (50) NULL,
+    [City]       VARCHAR (20) NULL,
+    [Country]    VARCHAR (20) NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
